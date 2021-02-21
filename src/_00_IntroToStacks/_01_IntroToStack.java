dd685b0e25a4d50e1935cc10239ddd057f1c69f7
Meme1sh_ <60296686+The-GitHub-Paradox@users.noreply.github.com>
2021-02-20 16:07:56 -0800

    e
    e

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,19 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+    	Stack<Double> stack = new Stack<Double>();
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-
+    	for(int i = 0; i > 100; i++){
+    		stack.push(5.0);
+    	}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-
+    	JOptionPane.showInputDialog("Choose a number between 0 and 100");
+    	JOptionPane.showInputDialog("Choose another number between 0 and 100");
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
-
+    	Double topOfStack = stack.pop();
+    	System.out.println(topOfStack);
+    	System.out.println(stack.pop());
 
         // EXAMPLE:
         // NUM 1: 65
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -19,3 +19,4 @@ public class _02_TextUndoRedo {
 
 
 }
+               
